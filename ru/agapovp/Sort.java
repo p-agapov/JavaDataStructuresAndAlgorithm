56531ebc31855de9821b30202118b4803521008c
Pavel Agapov <p.a.agapov@gmail.com>
2016-03-29 04:15:21 +0300

    Add Insertion sort

--- ru/agapovp/Sort.java
+++ ru/agapovp/Sort.java
@@ -1,6 +1,6 @@
 /** Class with static methods which implement basic sorting algorithms.
  * @autor Agapov Pavel
- * @version 1.01
+ * @version 1.11
  */
 package ru.agapovp;
 
@@ -26,5 +26,25 @@ public abstract class Sort {
         long stop = System.nanoTime();
 
         System.out.println("Spent time: " + (stop - start));
+    }
+    /** Insertion sort
+     * @param arr - One-dimensional unsorted integer array.
+     */
+    public static void insSort(int[] arr) {
+
+        long start = System.nanoTime();
+
+       for(int i = 1; i < arr.length; i++){
+           for(int j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
+               int b = arr[j - 1];
+               arr[j - 1] = arr[j];
+               arr[j] = b;
+           }
+       }
+
+        long stop = System.nanoTime();
+
+        System.out.println("Spent time: " + (stop - start));
+
     }
 }
